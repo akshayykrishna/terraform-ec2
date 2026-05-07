d48b1f34d5cc6d5b39b41b2ee97e135a352e67c3
akshayykrishna <aakshayykrishna@gmail.com>
2026-05-07 12:31:16 +0530

    updated with default security group

--- ec2.tf
+++ ec2.tf
@@ -10,40 +10,42 @@ provider "aws" {
 
 
 # Create a security group to allow SSH
-resource "aws_security_group" "example" {
-  name        = "example-sg"
-  description = "Allow SSH inbound traffic"
+# resource "aws_security_group" "example" {
+# name        = "example-sg"
+#  description = "Allow SSH inbound traffic"
 
 #ssh access
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # Open to all; restrict in production
-  }
+#  ingress {
+#   from_port   = 22
+#   to_port     = 22
+#    protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]   # Open to all; restrict in production
+# }
 
 # HTTP access
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # Allows web traffic
-  }
+# ingress {
+#   from_port   = 80
+#   to_port     = 80
+#   protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]   # Allows web traffic
+# }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+# egress {
+#   from_port   = 0
+#   to_port     = 0
+#   protocol    = "-1"
+#   cidr_blocks = ["0.0.0.0/0"]
+# }
+#}
 
 # Launch an EC2 instance
 resource "aws_instance" "example" {
   ami           = "ami-0eb38b817b93460ac"   # Amazon Linux 2 AMI (update per region)
   instance_type = "t2.micro"                # Free tier eligible
   key_name      = "mobaxterm"
-  security_groups = [aws_security_group.example.name]
+
+# Use the default security group  
+  vpc_security_group_ids = ["sg-0db484edc0a0c070e"]
 
   tags = {
     Name = "terrainstance"
